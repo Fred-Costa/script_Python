11612015e33ae93365a57da0ea2232718c9564ab
Fred <Fredericobrasilcosta@gmail.com>
2023-10-12 17:37:58 +0000

    upgrade programs

---
+++ programs_upgrade.py
@@ -0,0 +1,18 @@
+import subprocess
+
+# Open cmd as Admin
+run_cmd = "runas /user:frede cmd"
+
+# Execute 'winget upgrade'
+winget_upgrade_cmd = "winget upgrade"
+
+# Execute 'winget upgrade --all'
+winget_upgrade_all_cmd = "winget upgrade --all"
+
+# All in one string
+full_cmd = f"{run_cmd} /K {winget_upgrade_cmd} & {winget_upgrade_all_cmd}"
+
+# Execute
+subprocess.run(full_cmd, shell=True)
+
+
